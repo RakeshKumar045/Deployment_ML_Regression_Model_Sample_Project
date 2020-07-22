612af6b9ab58ccec2db1d4c9e1e5c99a64b0f0b7
rakesh <raka06184@gmail.com>
2020-07-22 12:37:43 +0530

    Added Salary Prediction project

--- Salary_Prediction/model.py
+++ Salary_Prediction/model.py
@@ -1,6 +1,6 @@
 # Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pickle
 
@@ -32,8 +32,8 @@ regressor = LinearRegression()
 regressor.fit(X, y)
 
 # Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+pickle.dump(regressor, open('model.pkl', 'wb'))
 
 # Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 print(model.predict([[2, 9, 6]]))
